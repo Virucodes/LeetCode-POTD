8d73cf2423504da1e3d0612d56fa1fdd8ce8b341
Virucodes <162709833+Virucodes@users.noreply.github.com>
2024-07-21 14:53:31 +0530

    Time: 70 ms (85.32%) | Memory: 63.9 MB (46.51%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++)
+        {
+            int index = abs(nums[i]);
+
+            if(nums[index]<0)
+            {
+                return index;
+            }
+            else
+            {
+                nums[index]*=-1;
+            }
+        }
+        return -1;
+    }
+};
